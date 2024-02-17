cc5830b6bd4710038ef644b262bc0aa07b436347
ferutizi <fer.utizi@gmail.com>
2024-02-17 00:03:24 -0300

    added anually state for changed price

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,51 @@
+'use client'
+
+import { useState } from 'react'
 import Card from './components/Card'
 
 export default function Home() {
+	const [anually, setAnually] = useState<boolean>(true)
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1>Our Pricing</h1>
-			<div>
-				<p>Annually</p>
+			<h1 className='text-grayishBlue text-2xl p-4 font-bold'>Our Pricing</h1>
+			<div
+				className='flex items-center text-darkGrayishBlue gap-4 pb-8 font-bold'
+				onClick={() => setAnually(!anually)}
+			>
+				<p className='text-lightGrayishBlue text-xs'>Annually</p>
 				<div>B</div>
-				<p>Monthly</p>
+				<p className='text-lightGrayishBlue text-xs'>Monthly</p>
 			</div>
 			<section className='flex items-center'>
-				<Card plan='Basic' anualPrice={199.99} monthlyPrice={19.99} storage='500 GB' users={2} sendUp={3} />
-				<Card plan='Professional' anualPrice={249.99} monthlyPrice={24.99} storage='1 TB' users={5} sendUp={10} highlight={true}/>
-				<Card plan='Master' anualPrice={399.99} monthlyPrice={39.99} storage='2 TB' users={10} sendUp={20} />
+				<Card
+					plan='Basic'
+					annualPrice={199.99}
+					monthlyPrice={19.99}
+					storage='500 GB'
+					users={2}
+					sendUp={3}
+					anually={anually} 
+				/>
+				<Card
+					plan='Professional'
+					annualPrice={249.99}
+					monthlyPrice={24.99}
+					storage='1 TB'
+					users={5}
+					sendUp={10}
+					anually={anually}
+					highlight={true} 
+				/>
+				<Card
+					plan='Master'
+					annualPrice={399.99}
+					monthlyPrice={39.99}
+					storage='2 TB'
+					users={10}
+					sendUp={20}
+					anually={anually} 
+				/>
 			</section>
-			
-			{/* &dollar; */}
-			
 		</main>
 	)
 }
