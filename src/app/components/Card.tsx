f0eafde4813705285017ecfe76473e88ac62231e
ferutizi <fer.utizi@gmail.com>
2024-02-17 00:02:36 -0300

    added colors variables

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,30 +1,31 @@
 interface CardProps {
   plan: string,
-  anualPrice: number,
+  annualPrice: number,
   monthlyPrice: number,
   storage: string,
   users: number,
   sendUp: number,
+	anually: boolean,
   highlight?: boolean,
 }
 
-export default function Card({plan, anualPrice, monthlyPrice, storage, users, sendUp, highlight}: CardProps) {
+export default function Card({plan, annualPrice, monthlyPrice, storage, users, sendUp, anually, highlight}: CardProps) {
 	const highlightStyles = 'h-96 text-white bg-indigo-400'
-	const normalStyles = 'h-80 text-black bg-white'
+	const normalStyles = 'h-80 text-gray-700 bg-white'
 	return(
 		<article className={`${highlight ? highlightStyles : normalStyles} flex flex-col justify-around text-center w-64 p-4 rounded-md`}>
-			<h2>{plan}</h2>
-			<span className="text-5xl font-extrabold">{monthlyPrice} {/* {anualPrice} */}</span>
+			<h2 className={`text-sm ${highlight ? 'text-white' : 'text-grayishBlue'} font-bold`}>{plan}</h2>
+			<span className="text-5xl font-extrabold flex justify-center items-center gap-2"><span className="text-3xl">$</span>{anually ? annualPrice : monthlyPrice} {/* {anualPrice} */}</span>
 			<div>
+				<hr className="text-veryLightGrayishBlue bg-veryLightGrayishBlue" />
+				<p className={`${highlight ? 'text-white' : 'text-grayishBlue'} py-2 text-xs font-semibold`}>{storage} Storage</p>
 				<hr />
-				<p className="py-2 text-xs">{storage} Storage</p>
+				<p className={`${highlight ? 'text-white' : 'text-grayishBlue'} py-2 text-xs font-semibold`}>{users} Users Allowed</p>
 				<hr />
-				<p className="py-2 text-xs">{users} Users Allowed</p>
-				<hr />
-				<p className="py-2 text-xs">Send up to {sendUp} GB</p>
+				<p className={`${highlight ? 'text-white' : 'text-grayishBlue'} py-2 text-xs font-semibold`}>Send up to {sendUp} GB</p>
 				<hr />
 			</div>
-			<button type="button" className={`${highlight ? 'border-white' : 'border-indigo-500'} w-full text-center py-2 border rounded-md`}>Learn More</button>
+			<button type="button" className={`${highlight ? 'border-white text-white' : 'border-indigo-500 text-indigo-500'} w-full text-center py-1 border rounded-md text-xs font-semibold`}>LEARN MORE</button>
 		</article>
 	)
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
 				'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
 			},
+			colors: {
+				'linearGradient': 'hsl(236, 72%, 79%) to hsl(237, 63%, 64%)',
+				'veryLightGrayishBlue': 'hsl(240, 78%, 98%)',
+				'lightGrayishBlue': 'hsl(234, 14%, 74%)',
+				'grayishBlue': 'hsl(233, 13%, 49%)',
+				'darkGrayishBlue': 'hsl(232, 13%, 33%)'
+			},
+			fontSize: {
+				'sm': '15px',
+				'xs': '12px'
+			},
+			padding: {
+				'2': '12px'
+			}
 		},
 	},
 	plugins: [],
